f27f82d7ee77f1f720f3455c9c99196e53bf705a
damoblinkz2 <adedamolaolatomide@gmail.com>
2022-12-30 10:34:21 +0100

    add all

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// =========FILTER LINK ACTIVE====================
+
+var selector = ".new-p-heading li";
+
+$(selector).on("click", function () {
+  $(selector).removeClass("active");
+  $(this).addClass("active");
+});
+
+// ==================BOX FILTER========================
+
+$(document).ready(function () {
+  $(".new-p-heading li").click(function () {
+    const value = $(this).attr("data-filter");
+
+    if (value == "all") {
+      $(".new-product-box").show("1000");
+    } else {
+      $(".new-product-box")
+        .not("." + value)
+        .hide("1000");
+      $(".new-product-box")
+        .filter("." + value)
+        .show("1000");
+    }
+  });
+});
